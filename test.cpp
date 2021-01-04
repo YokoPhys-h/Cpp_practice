eea661f15dd94d0897d372ebf8cf04bbf07effbe
YokoPhys-h <yoko.haruru.phy@outlook.jp>
2021-01-04 19:51:55 +0900

    snipettsを追加

---
+++ test.cpp
@@ -0,0 +1,22 @@
+/**
+*    author:  Haruto Yokoyama
+*    created: 2021/01/04 19:13:40
+**/
+
+#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    cout << "test" << endl;
+
+   return 0;
+}
